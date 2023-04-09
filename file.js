a3195628213cc30b2208d20183b531083a2e429e
Tisha Gupta <tishaguptatop@gmail.com>
2023-04-09 19:00:20 +0530

    first commit

---
+++ file.js
@@ -0,0 +1,42 @@
+// My Way
+
+const activeemoji = document.querySelector(".active");
+const openemoji = document.querySelector(".open");
+// console.log(activeemoji.textContent);
+// console.log(openemoji.textContent);
+
+activeemoji.addEventListener("click",(event)=>{
+    // event.preventDefault();
+    activeemoji.style.display = "none";
+    openemoji.style.display = "block";
+
+}) 
+
+openemoji.addEventListener("click",(event)=>{
+    // event.preventDefault();
+    openemoji.style.display = "none";
+    activeemoji.style.display = "block";
+
+}) 
+
+
+
+// From Video 
+// const closedemoji = document.querySelector(".closed");
+// const openemoji = document.querySelector(".open");
+
+// // Add event Listener
+// closedemoji.addEventListener("click",() =>{
+//   if (openemoji.classList.contains('open')) {
+//     openemoji.classList.add('active');
+//     closedemoji.classList.remove('active');
+//   }
+
+// })
+
+// openemoji.addEventListener('click',()=>{
+//     if (closedemoji.classList.contains('closed')) {
+//         closedemoji.classList.add('active');
+//         openemoji.classList.remove('active');
+//     }
+// })
